0c0fb02b1da14512b2bcc0cdad0c5804625535c0
VengoVE <v726562@gmail.com>
2024-01-27 21:51:46 +0300

    Task6

---
+++ Tyuiu.VengoVE.Sprint3.Task6.V12.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tyuiu.VengoVE.Sprint3.Task6.V12.Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tyuiu.VengoVE.Sprint3.Task6.V12.Test")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("87a1ba11-8fe6-4f7f-bb41-a546219af107")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
--- Tyuiu.VengoVE.Sprint3.Task6.V12/Program.cs
+++ Tyuiu.VengoVE.Sprint3.Task6.V12/Program.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-using Tyuiu.VengoVE.Sprint3.Task5.V9.Lib;
+using Tyuiu.VengoVE.Sprint3.Task6.V12.Lib;
 
-namespace Tyuiu.VengoVE.Sprint3.Task5.V9
+namespace Tyuiu.VengoVE.Sprint3.Task6.V12
 {
     class Program
     {
@@ -16,33 +16,29 @@ namespace Tyuiu.VengoVE.Sprint3.Task5.V9
             Console.Title = "Спринт #3 | Выполнил: Вэнго В. Э. | ИСПб-23-1";
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* Спринт #3                                                               *");
-            Console.WriteLine("* Тема: Вложенные циклы                                                   *");
-            Console.WriteLine("* Задание #5                                                              *");
-            Console.WriteLine("* Вариант #9                                                              *");
+            Console.WriteLine("* Тема: Обработка целочисленной информации                                *");
+            Console.WriteLine("* Задание #6                                                              *");
+            Console.WriteLine("* Вариант #12                                                             *");
             Console.WriteLine("* Выполнил: Вэнго В. Э. | ИСПб-23-1                                       *");
             Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Напишите программу, которая ищет среди целых чисел, принадлежащих       *");
+            Console.WriteLine("* числовому отрезку [9, 18] сумму  всех делителей.                        *");
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
             Console.WriteLine("***************************************************************************");
 
-            int x = 5;
-            int startValue1 = 1;
-            int stopValue1 = 3;
-            int startValue2 = 1;
-            int stopValue2 = 14;
+            int startValue = 9;
+            int stopValue = 18;
 
-            
-            Console.WriteLine("Переменная X = " + x);
-            Console.WriteLine("Старт шага первой суммы ряда = " + startValue1);
-            Console.WriteLine("Конец шага первой суммы ряда = " + stopValue1);
-            Console.WriteLine("Старт шага второй суммы ряда = " + startValue2);
-            Console.WriteLine("Конец шага второй суммы ряда =" + stopValue2);
+            Console.WriteLine("Начало отрезка = " + startValue);
+            Console.WriteLine("Конец отрезка = " + stopValue);
 
             Console.WriteLine("***************************************************************************");
             Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
             Console.WriteLine("***************************************************************************");
 
-            Console.WriteLine("Сумма ряда = " + ds.GetSumSumSeries(x, startValue1, stopValue1, startValue2, stopValue2));
+            Console.WriteLine("Сумма делителей = " + ds.GetSumTheDivisors(startValue, stopValue));
             Console.ReadKey();
         }
     }
